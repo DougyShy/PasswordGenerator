bf6e66efde4580259b3654c0815195a7dc243db7
Doug Scheible <doug_scheible@yahoo.com>
2024-02-09 00:09:15 -0600

    Fixed cancel bug after first retry

--- script.js
+++ script.js
@@ -15,17 +15,20 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
 
   // Initialize number of characters or if user cancels start over
-  numChars = window.prompt("How many characters would you like the password to be? (8 - 128)", 8);
+  numChars = window.prompt("How many characters would you like the password to be? (8 - 128)", "8");
   console.log(numChars);
-  if (numChars == null) {
-    // console.log("QUITTING");
+  if ((numChars == null)) {
+    console.log("QUITTING");
     return false;
   }
 
-  // Make sure numbers fall in range. Only accept numbers between 8 and 128
+  // Make sure numbers fall in range. Only accept NUMBERS between 8 and 128
   while((numChars < 8  || numChars > 128) || isNaN(numChars)) {
     numChars = window.prompt("How many characters would you like the password to be? (8 - 128)");
-    console.log(numChars); 
+    if ((numChars == null)) {
+      console.log("QUITTING");
+      return false;
+    }
   }
 
   // Check to see what types of character the user wants to use in their random password
@@ -65,20 +68,19 @@ function generatePassword() {
 
   // If the user picked 'no' for ALL criteria to include tell them to restart and why
   if(optionsChosen.length == 0) {
-    alert("You chose for the password to have no criteria. Please retry.");
-    return "No criteria specified.";
+    alert("You chose for the password to have NO (0) criteria. Please retry.");
+    return "No (0) criteria specified.";
   }
 
-  // Now randomize the rest
+  // Now randomize the rest of the characters
   for (i = (tempPassword.length); i < numChars; i++) {
     tempPassword.push(getRandomCharacter(optionsChosen[Math.floor(Math.random() * optionsChosen.length)]));
-    console.log(getRandomCharacter(optionsChosen[Math.floor(Math.random() * optionsChosen.length)]));
   }  
 
-  // ** JUST FOR FUN -- RANDOMIZE THE RANDOM ARRAY TO MAKE SURE THAT ALL TYPES DON'T *AUTOMATICALLY* APPEAR INLINE FOR FIRST ITEMS - NOT THAT IT MATTERS BUT JUST IN CASE #CYBERSECURITY
-  console.log(tempPassword);
+  // ** JUST FOR FUN -- RANDOMIZE THE RANDOM ARRAY TO MAKE SURE THAT ALL TYPES DON'T *AUTOMATICALLY* APPEAR INLINE FOR FIRST (4) ITEMS - NOT THAT IT MATTERS BUT JUST IN CASE #CYBERSECURITY
+  //console.log(tempPassword); -- original
   tempPassword.sort(() => Math.random() - 0.5);
-  console.log(tempPassword);
+  //console.log(tempPassword); -- after sort
   
   // return generated password array as a string
   return tempPassword.join('');
@@ -98,26 +100,19 @@ function getRandomCharacter(format) {
 }
 
 function getRandomLowercaseLetter() {
-  var randomLowercaseLetter = "";
-  randomLowercaseLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
-  return randomLowercaseLetter;
+  return alphabet[Math.floor(Math.random() * alphabet.length)];
 }
 
 function getRandomUppercaseLetter() {
-  var randomUppercaseLetter = "";
-  randomUppercaseLetter = alphabet[Math.floor(Math.random() * alphabet.length)].toUpperCase();
-  return randomUppercaseLetter;
+  return alphabet[Math.floor(Math.random() * alphabet.length)].toUpperCase();
 }
 
 function getRandomNumber() {
-  var randomNumber = Math.floor(Math.random() * 10);
-  return randomNumber;
+  return Math.floor(Math.random() * 10);
 }
 
 function getRandomSpecialCharacter () {
-  var randomSpecialCharacter = "";
-  randomSpecialCharacter = specialCharsOptions[Math.floor(Math.random() * specialCharsOptions.length)];
-  return randomSpecialCharacter;
+  return specialCharsOptions[Math.floor(Math.random() * specialCharsOptions.length)];
 }
 
 // Add event listener to generate button
